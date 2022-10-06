66bc8f9942fbbe73fd2bb77f180957afd7345fec
Henrik Wolf <henrik-wolf@freenet.de>
2022-10-06 16:22:06 +0200

    added some "testcases"

--- test/plotting.jl
+++ test/plotting.jl
@@ -53,6 +53,6 @@ end
     buildings = load_british_shapefiles(joinpath([homedir(), path]); bbox=(minlat=52.89, minlon=-1.2, maxlat=52.92, maxlon=-1.165))
     shadows = cast_shadow(buildings, :height_mean, [1.0, -0.5, 0.6])
     m = polygons(shadows.geometry; figure_params=Dict(:location=>[52.904, -1.18], :zoom_start=>14))
-
-    
+    polygons!(m, buildings.geometry)
+    display(m)
 end
