695feb341d91cc9d3f76a8fe4df0c58a9e564d86
Henrik Wolf <henrik-wolf@freenet.de>
2022-10-07 18:59:00 +0200

    added tree loader and some plotting

--- test/current.jl
+++ test/current.jl
@@ -1,19 +1,31 @@
 using MinistryOfCoolWalks
-FoliumMap()
 
-path = "Desktop/Masterarbeit/data/Nottingham/Nottingham.shp"
 using ShadowGraphs
 using CompositeBuildings
-buildings = load_british_shapefiles(joinpath([homedir(), path]); bbox=(minlat=52.89, minlon=-1.2, maxlat=52.92, maxlon=-1.165))
+using TreeLoaders
+
+
+datapath = joinpath(homedir(), "Desktop/Masterarbeit/data/Nottingham/")
+buildings = load_british_shapefiles(joinpath(datapath, "Nottingham.shp"); bbox=(minlat=52.89, minlon=-1.2, maxlat=52.92, maxlon=-1.165))
 shadows = cast_shadow(buildings, :height_mean, [1.0, -0.5, 0.6])
+trees = load_nottingham_trees(joinpath(datapath, "trees/trees_full_rest.csv"); bbox=(minlat=52.89, minlon=-1.2, maxlat=52.92, maxlon=-1.165))
+
+
+begin
+    m = polygons(shadows.geometry; 
+    figure_params=Dict(:location=>[52.904, -1.18], :zoom_start=>14, :tiles=>"CartoDB PositronNoLabels"),
+    color="black", fill=true, stroke=false, fill_opacity=0.4)
+    circles!(m, trees.lon, trees.lat; radius=5, color="#68bd61", stroke=false, fill=true, fill_opacity=0.3)
+    polygons!(m, buildings.geometry; fill=true, stroke=false, fill_opacity=1)
+end
+
+_, g = shadow_graph_from_file(joinpath(datapath, "test_nottingham.json"))
 begin
     m = polygons(shadows.geometry; 
         figure_params=Dict(:location=>[52.904, -1.18], :zoom_start=>14, :tiles=>"CartoDB PositronNoLabels"),
         color="black", fill=true, stroke=false, fill_opacity=0.4)
+    circles!(m, trees.lon, trees.lat; radius=5, color="#68bd61", stroke=false, fill=true, fill_opacity=0.3)
     polygons!(m, buildings.geometry; fill=true, stroke=false, fill_opacity=1)
+    graph_node_circles!(m, g; radius=1, color="#e2b846")
+    graph_edges!(m, g; weight=2, color="#e56c6c", opacity=0.7)
 end
-
-path = "Desktop/Masterarbeit/data/Nottingham/test_nottingham.json"
-_, g = shadow_graph_from_file(joinpath([homedir(), path]))
-
-MinistryOfCoolWalks.get_vert_coords(g)
