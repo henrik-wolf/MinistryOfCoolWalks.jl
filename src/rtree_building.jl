044038b7f4a9b954a79869b5cc52492233c0b88a
Henrik Wolf <henrik-wolf@freenet.de>
2022-11-03 18:27:21 +0100

    added progressbar to rtee builder

--- src/rtree_building.jl
+++ src/rtree_building.jl
@@ -1,6 +1,6 @@
 function build_rtree(geocolumn)
     rt = RTree{Float64, 2}(Int, NamedTuple{(:orig, :prep), Tuple{ArchGDAL.IGeometry{ArchGDAL.wkbPolygon}, ArchGDAL.IPreparedGeometry}})
-    for (i, geom) in enumerate(geocolumn)
+    @showprogress 1 "building r tree" for (i, geom) in enumerate(geocolumn)
         bbox = rect_from_geom(geom)
         insert!(rt, bbox, i, (orig=geom, prep=ArchGDAL.preparegeom(geom)))
     end
