2f32874ed0d5e29cc06481a41b1bf31c9e1c3658
Henrik Wolf <henrik-wolf@freenet.de>
2022-11-08 18:13:28 +0100

    added shadow casting for trees

--- src/ShadowCasting.jl
+++ src/ShadowCasting.jl
@@ -10,6 +10,57 @@ function shadow_cleanup(::GeometryCollectionTrait, shadow)
     end
 end
 
+# stolen from LinearAlgebra
+function cross(a, b)
+    if !(length(a) == length(b) == 3)
+        throw(DimensionMismatch("cross product is only defined for vectors of length 3"))
+    end
+    a1, a2, a3 = a
+    b1, b2, b3 = b
+    [a2*b3-a3*b2, a3*b1-a1*b3, a1*b2-a2*b1]
+end
+
+function cast_shadow(tree_df, param_getter::Function, sun_direction::AbstractArray)
+    @assert sun_direction[3] > 0 "the sun is below or on the horizon. Everything is in shadow."
+    project_local!(tree_df.pointgeom, metadata(tree_df, "center_lon"), metadata(tree_df, "center_lat"))
+
+    # construct right hand system with sun direction:
+    v1 = norm(cross(sun_direction, [1,0,0]))
+    v2 = norm(cross(v1, sun_direction))
+
+    # create projection of silhouette centered on (0,0) at height 0
+    n = 8
+    angles = LinRange(0, 2π, n+1)
+    x_plane = cos.(angles)'
+    y_plane = sin.(angles)'
+
+    points = x_plane .* v1 + y_plane .* v2
+
+    projected_points = [x[1:2] - sun_direction[1:2] * x[3]/sun_direction[3] for x in eachcol(points)]
+    
+    # find offset vector
+    offset_vector = - sun_direction[1:2] ./ sun_direction[3]
+
+    shadow_df = DataFrame(geometry=ArchGDAL.IGeometry{ArchGDAL.wkbPolygon}[], id=typeof(tree_df.id)())
+    for key in metadatakeys(tree_df)
+        metadata!(shadow_df, key, metadata(tree_df, key); style=:note)
+    end
+    
+    @showprogress 1 for row in eachrow(tree_df)
+        # discover tree parameters
+        x, y, h, r = param_getter(row)
+
+        # create shadow
+        shadow = ArchGDAL.createpolygon([(x + h*offset_vector[1] + r*p[1], y + h*offset_vector[2] + r*p[2]) for p in projected_points])
+        reinterp_crs!(shadow, ArchGDAL.getspatialref(row.pointgeom))
+        push!(shadow_df, [shadow, row.id])
+    end
+
+    project_back!(tree_df.pointgeom)
+    project_back!(shadow_df.geometry)
+    return shadow_df
+end
+
 function cast_shadow(buildings_df, height_key, sun_direction::AbstractArray)
     @assert sun_direction[3] > 0 "the sun is below or on the horizon. Everything is in shadow."
     #@info "this function assumes you geometry beeing in a suitable crs to do projections"
